a303f7d5c61bb32dc85804be92d4abfce061bdcd
kir2357 <56784991+kir2357@users.noreply.github.com>
2022-05-22 21:32:48 +0900

    連打を抑える

--- pad.py
+++ pad.py
@@ -7,8 +7,8 @@ ADB_PATH = '../platform-tools/adb'
 def range_tap(x1,y1,x2,y2):
     _x1 = random.randint(x1,x2)
     _y1 = random.randint(y1,y2)
-    _x2 = _x1 + random.randint(-5,5)
-    _y2 = _y1 + random.randint(-5,5)
+    _x2 = _x1 + random.randint(-3,3)
+    _y2 = _y1 + random.randint(-3,3)
     _msec = random.randint(100,200)
     try:
         #subprocess.check_output([ADB_PATH, 'shell', 'input','touchscreen', 'tap', str(_x), str(_y)])
@@ -30,72 +30,84 @@ def main():
         # 早送りボタンは常にタップ
         print(phase,counter)
         if phase=="rematch":
-            range_tap(334,1827,744,1911)
+            if counter < 3:
+                range_tap(334,1827,744,1911)
 
-            if counter >= 13:
+            if counter >= 15:
                 phase = "battle1"
                 counter = 0
             else:
                 counter += 1
 
         elif phase=="battle1":
-            range_tap(12,1192,166,1340)
+            if counter < 4:
+                range_tap(12,1192,166,1340)
 
-            if counter >= 10:
+            if counter >= 12:
                 phase = "battle2"
                 counter = 0
             else:
                 counter += 1
 
         elif phase=="battle2":
-            range_tap(190,1192,346,1340)
+            if counter < 4:
+                range_tap(190,1192,346,1340)
 
-            if counter >= 10:
+            if counter >= 12:
                 phase = "battle3"
                 counter = 0
             else:
                 counter += 1
 
         elif phase=="battle3":
-            range_tap(366,1192,524,1340)
+            
+            if counter < 4:
+                range_tap(366,1192,524,1340)
 
-            if counter >= 10:
+            if counter >= 12:
                 phase = "battle4"
                 counter = 0
             else:
                 counter += 1
 
         elif phase=="battle4":
-            range_tap(549,1192,702,1340)
+            
+            if counter < 4:
+                range_tap(549,1192,702,1340)
 
-            if counter >= 8:
+            if counter >= 10:
                 phase = "battle5"
                 counter = 0
             else:
                 counter += 1
 
         elif phase=="battle5":
-            range_tap(727,1192,888,1340)
+            
+            if counter < 4:
+                range_tap(727,1192,888,1340)
 
-            if counter >= 8:
+            if counter >= 10:
                 phase = "battle6"
                 counter = 0
             else:
                 counter += 1
 
         elif phase=="battle6":
-            range_tap(906,1192,1065,1340)
+            
+            if counter < 4:
+                range_tap(906,1192,1065,1340)
 
-            if counter >= 8:
+            if counter >= 10:
                 phase = "clear"
                 counter = 0
             else:
                 counter += 1
 
         elif phase=="clear":
-            range_tap(424,2039,657,2118)
+            if counter < 4:
+                range_tap(424,2039,657,2118)
 
-            if counter >= 3:
+            if counter >= 5:
                 phase = "get_wait"
                 counter = 0
             else:
@@ -103,9 +115,10 @@ def main():
 
         elif phase=="get_wait":
             ## 長押し処理やがめんどい
-            range_tap(906,1192,1065,1340)
+            if counter < 4:
+                range_tap(906,1192,1065,1340)
 
-            if counter >= 13:
+            if counter >= 15:
                 phase = "rematch"
                 counter = 0
             else:
